8e4f0c28e4151d7e5120052f5be06e5a0aebab6b
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-09 07:41:09 +1200

    Add project files.

---
+++ c# 8 of june 2024/Program.cs
@@ -0,0 +1,20 @@
+﻿int num = 0;
+//increment a number by 1
+num++;
+Console.WriteLine("num is "+ num);
+//increment a number by 15
+num += 15;
+Console.WriteLine($"num is {num}");
+//decrement the num by 5
+num -= 5;
+Console.WriteLine($"num is {num}");
+//decrement a number by 1
+num--;
+Console.WriteLine($"num is {num}");
+//multiply the number by 12
+num *= 12;
+Console.WriteLine($"num is {num}");
+//divide the number by 6
+num /= 6;
+Console.WriteLine($"num is {num}"  );
+    
